6ff76bd5ae6c245b76e4deee72d267a771220864
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-01-18 18:58:20 -0500

    code changed to async await

--- app.js
+++ app.js
@@ -4,33 +4,29 @@ const btn = document.querySelector(".btn");
 const content = document.querySelector(".content");
 const container = document.querySelector(".container");
 const img = document.querySelector(".container img");
-btn.addEventListener("click", () => {
-  getJoke();
+btn.addEventListener("click", async () => {
+  try {
+    const data = await fetch(url);
+    const response = await data.json();
+    shakeChuck(response);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // console.log(data);
+  // console.log(response);
 });
 //add fetch
 
-function getJoke() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      // console.log(xhr.responseText);
-      img.classList.add("shake-img");
-      const time = Math.ceil(Math.random() * 1000);
-
-      const { value: data } = JSON.parse(xhr.responseText);
-
-      content.textContent = data;
-      setTimeout(() => {
-        img.classList.remove("shake-img");
-      }, time);
-      //
-    } else {
-      console.log({ status: xhr.status, text: xhr.statusText });
-    }
-  };
-
-  xhr.send();
-    // console.log(xhr);
+function shakeChuck({ value: data }) {
+  img.classList.add("shake-img");
+  const time = Math.ceil(Math.random() * 1000);
+  content.textContent = data;
+  setTimeout(() => {
+    img.classList.remove("shake-img");
+  }, time);
 }
+
+// console.log(xhr);
+
 // console.log(getJoke());
